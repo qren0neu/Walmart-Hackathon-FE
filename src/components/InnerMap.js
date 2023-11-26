35058a2682b64732053a57cb5b940b487b8a0403
Zeyu Liao <liao.ze@northeastern.edu>
2023-11-25 21:19:24 -0500

    Innitialize the project with InnerMap

---
+++ src/components/InnerMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InnerMap() {
+  return (
+    <div>
+      <iframe src="map/index.html" width="100%" height="850px"></iframe>
+    </div>
+  );
+}
+
+export default InnerMap;
